602f1cae600d954d70f6e4d7dc3eb72715a77ed1
Richard Dantas <adriano.richard@netimoveis.com>
2024-08-18 23:57:36 -0300

    atualizando login page

--- src/shared/components/login/Login.tsx
+++ src/shared/components/login/Login.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { Box, Button, Card, CardActions, CardContent, CircularProgress, TextField, Typography } from '@mui/material';
+import { Avatar, Box, Button, Card, CardActions, CardContent, Checkbox, CircularProgress, CssBaseline, FormControlLabel, Grid, Link, Paper, TextField, Typography } from '@mui/material';
 import * as yup from 'yup';
-
+import Image from 'next/image';
 import { useAuthContext } from '../../contexts';
+import { Copyright } from '@mui/icons-material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 
 const loginSchema = yup.object().shape({
@@ -54,53 +56,88 @@ export const Login: React.FC<ILoginProps> = ({ children }) => {
   );
 
   return (
-    <Box width='100vw' height='100vh' display='flex' alignItems='center' justifyContent='center'>
+    <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              'url("/login-images/35.png")',
 
-      <Card>
-        <CardContent>
-          <Box display='flex' flexDirection='column' gap={2} width={250}>
-            <Typography variant='h6' align='center'>Identifique-se</Typography>
-
-            <TextField
-              fullWidth
-              type='email'
-              label='Email'
-              value={email}
-              disabled={isLoading}
-              error={!!emailError}
-              helperText={emailError}
-              onKeyDown={() => setEmailError('')}
-              onChange={e => setEmail(e.target.value)}
-            />
-
-            <TextField
-              fullWidth
-              label='Senha'
-              type='password'
-              value={password}
-              disabled={isLoading}
-              error={!!passwordError}
-              helperText={passwordError}
-              onKeyDown={() => setPasswordError('')}
-              onChange={e => setPassword(e.target.value)}
-            />
-          </Box>
-        </CardContent>
-        <CardActions>
-          <Box width='100%' display='flex' justifyContent='center'>
-
-            <Button
-              variant='contained'
-              disabled={isLoading}
-              onClick={handleSubmit}
-              endIcon={isLoading ? <CircularProgress variant='indeterminate' color='inherit' size={20} /> : undefined}
-            >
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'left',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
               Entrar
-            </Button>
-
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Lembrar"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Entrar
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Esqueceu sua senha?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Não possui uma conta? Cadastre-se"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
           </Box>
-        </CardActions>
-      </Card>
-    </Box>
+        </Grid>
+      </Grid>
   );
 };
